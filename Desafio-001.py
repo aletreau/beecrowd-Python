9953ea12b09f0aaafdbafdabd84bea6216d94ceb
aletreau <boulitreau@gmail.com>
2022-06-19 11:14:45 -0300

    Atualização da solução

--- Desafio-001.py
+++ Desafio-001.py
@@ -15,34 +15,50 @@ reajuste = r
 percentual = p
 '''
 
+#x = float(input())
+#x = "{:.2f}".format(float(input()))
+
+
 x = float(input())
 
-if x <= 400.00:
-        s = x * 1.15
-        r = s - x
-        p=15
-if 400.01 <= x <= 800.00:
-        s = x * 1.12
-        r = s - x
-        p=12
-if 800.01 <= x <=  1200.00:
-        s = x * 1.10
-        r = s - x
-        p=10
-if 1200.01 <= x <= 2000.00:
-        s = x*1.07
-        r = s-x
-        p=7
-if x > 2000.00:
-        s = x*1.04
-        r = s-x
-        p=4
+
+if  0 <= x <=  400.00:
+    s = x * 1.15
+    r = s - x
+    p=15
     
+elif 400.01 <= x <= 800.00:
+    s  = x * 1.12
+    r = s - x
+    p=12
+    
+elif 800.01 <= x <=  1200.00:
+    s = x * 1.10
+    r = s - x
+    p=10
+    
+elif 1200.01 <= x <= 2000.00:
+    s = x*1.07
+    r = s-x
+    p=7
+    
+elif x > 2000.00:
+    s = x*1.04
+    r = s-x
+    p=4
+            
+
+print(('Novo Salario: {:.2f}'.format(s)),('\nReajuste ganho: {:.2f}'.format(r)),'\nEm percentual: ',p,'%')
 
 
-print('Novo Salário: {:.2f}'.format(s))
-print('Reajuste ganho: {:.2f}'.format(r))
-print('Em percentual: {} %'.format(p))
+
+#print(('Novo Salario: {:.2f}'.format(s)),('\nReajuste ganho: {:.2f}'.format(r)),('\nEm percentual: {} %'.format(p)))
+
+
+
+
+#print('Em percentual: {} %'.format(p))
+
 
 
 
