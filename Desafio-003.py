087896576b14a63b65c2e40cbe0f2773398a9214
aletreau <boulitreau@gmail.com>
2022-06-19 23:40:51 -0300

    Inicio

---
+++ Desafio-003.py
@@ -0,0 +1,12 @@
+'''Distância Entre Dois Pontos
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1'''
+import math
+from posixpath import split
+
+
+
+x2, x1= input().split()
+y2, y1= input().split()
+print(x1," ",x2," ",y1," ",y2)
