b4d81f44ed1bd23a4aabbf6f83d637a666e0f048
aletreau <boulitreau@gmail.com>
2022-07-01 17:19:57 -0300

    exercicio 10

---
+++ Desafio-010.py
@@ -0,0 +1,23 @@
+'''beecrowd | 1035
+Teste de Seleção 1'''
+
+'''Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D 
+for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, 
+ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", 
+senão escrever "Valores nao aceitos".'''
+
+from socket import if_indextoname
+
+
+A,B,C,D=input(5 6 7 8).split()
+
+#valor = A,B,C,D
+
+#if (B>C) and (D>A) and ((C+D)>(A+B)) and (C ,D > 0) and (A%2==0):
+
+if B>C:
+    print("TEste")
+else:
+    print("error")
+
+
