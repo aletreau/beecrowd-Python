7b0270e26550fca0577d1b68f2ccb975449de4d8
aletreau <boulitreau@gmail.com>
2022-07-07 13:41:25 -0300

    Baskara resolvido

--- Desafio-011.py
+++ Desafio-011.py
@@ -5,7 +5,7 @@ Timelimit: 1
 Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. 
 Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”,
 caso haja uma divisão por 0 ou raiz de numero negativo.'''
-
+import math
 A, B, C = input().split()
 
 print(A, B, C)
@@ -14,9 +14,11 @@ print(A, B, C)
 print(float(B)**2)
 print(4*float(A))
 print((-4*float(A))*float(B))
-delta = (float(B)**2) - 4*float(A)*float(B)
+delta = (float(B)**2) - 4*float(A)*float(C)
 
-#delta = delta = (b ** 2) - 4 * a * c
-
+x1 = (-float(B) + delta ** (1 / 2)) / (2 * float(A))
+x2 = (-float(B) - delta ** (1 / 2)) / (2 * float(A))
 
 print(delta)
+print(math.sqrt(delta))
+print(x1," ",x2)
